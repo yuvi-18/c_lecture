4f759137f49c5bf52398464b2d15d95db28d5b57
Yuvraj <fcyuvraj18@gmail.com>
2024-10-22 18:53:28 +0530

    update

--- forloop.c
+++ forloop.c
@@ -23,66 +23,66 @@ int main()
     //     printf("2 * %d = %d\n",i,2*i );
 
     // }
-    // int m, n;
-    // printf("Enter number of rows for the matrix: ");
-    // scanf("%d", &m);
-    // printf("Enter number of columns for the matrix: ");
-    // scanf("%d", &n);
+    int m, n;
+    printf("Enter number of rows for the matrix: ");
+    scanf("%d", &m);
+    printf("Enter number of columns for the matrix: ");
+    scanf("%d", &n);
 
-    // for (int j = 1; j <= m; j++)
-    // {
-    //     printf("%d ", j);
-    // }
-    // for (int i = 1; i < n; i++)
-    // {
-    //     printf("\n");
-    //     for (int j = 1; j <= m; j++)
-    //     {
-    //         printf("%d ", j);
-    //     }
-    // }
-    int month;
-    printf("Enter month: ");
-    scanf("%d", &month);
-
-    switch (month)
+    for (int j = 1; j <= m; j++)
     {
-    case 1:
-        printf("Jan");
-        break;
-    case 2:
-        printf("Feb");
-        break;
-    case 3:
-        printf("March");
-        break;
-    case 4:
-        printf("April");
-        break;
-    case 5:
-        printf("May");
-        break;
-    case 6:
-        printf("June");
-        break;
-    case 7:
-        printf("July");
-        break;
-    case 8:
-        printf("Aug");
-        break;
-    case 9:
-        printf("Sep");
-        break;
-    case 10:
-        printf("Oct");
-        break;
-    case 11:
-        printf("Nov");
-        break;
-    default:
-    printf("Dec");
-        break;
+        printf("%d ", j);
     }
+    for (int i = 1; i < n; i++)
+    {
+        printf("\n");
+        for (int j = 1; j <= m; j++)
+        {
+            printf("%d ", j);
+        }
+    }
+    // int month;
+    // printf("Enter month: ");
+    // scanf("%d", &month);
+
+    // switch (month)
+    // {
+    // case 1:
+    //     printf("Jan");
+    //     break;
+    // case 2:
+    //     printf("Feb");
+    //     break;
+    // case 3:
+    //     printf("March");
+    //     break;
+    // case 4:
+    //     printf("April");
+    //     break;
+    // case 5:
+    //     printf("May");
+    //     break;
+    // case 6:
+    //     printf("June");
+    //     break;
+    // case 7:
+    //     printf("July");
+    //     break;
+    // case 8:
+    //     printf("Aug");
+    //     break;
+    // case 9:
+    //     printf("Sep");
+    //     break;
+    // case 10:
+    //     printf("Oct");
+    //     break;
+    // case 11:
+    //     printf("Nov");
+    //     break;
+    // default:
+    // printf("Dec");
+    //     break;
+    // }
     return 0;
 }
