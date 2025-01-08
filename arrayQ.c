525afcbc90b0f8f61cd70a8834b22b17a95a25d5
Yuvraj <fcyuvraj18@gmail.com>
2025-01-09 01:41:27 +0530

    Create arrayQ.c

---
+++ arrayQ.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int arr[]= {1,2,3,4,5,6}, size;
+    size = sizeof(arr) / sizeof(arr[0]);
+    printf("size: %d \n", size);
+
+    for (int i = size -1; i >= 0; i--)
+    {
+        printf("%d \n", arr[i]);
+    }
+    return 0;
+        
+}
